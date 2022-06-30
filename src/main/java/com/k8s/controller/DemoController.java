a2daac2c64b4f3f5d4564974f35202d13d55d0f2
btons <btonslhu@gmail.com>
2022-06-30 16:28:33 +0800

    第一版程式
    第一版程式

---
+++ src/main/java/com/k8s/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.k8s.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+"! I'm springboot-web-demo controller!";
+    }
+    
+}
