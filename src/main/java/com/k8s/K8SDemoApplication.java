87bb6c35771fa1fad7f5d848f931c015e115eef2
btons <btonslhu@gmail.com>
2022-12-21 13:58:53 +0800

    第四版更新

--- src/main/java/com/k8s/K8SDemoApplication.java
+++ src/main/java/com/k8s/K8SDemoApplication.java
@@ -1,41 +1,12 @@
 package com.k8s;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.junit.jupiter.api.Assertions;
-
-import com.k8s.core.Article;
-import com.k8s.core.Reader;
-import com.k8s.core.Writer;
-
-//@SpringBootApplication
+@SpringBootApplication
 public class K8SDemoApplication {
 
 	public static void main(String[] args) {
-	    Reader reader=null;
-	    Writer writer=null;
-	    
-        try {
-            reader = new Reader();
-            
-            List<Article> result = reader.getList("Gossiping");
-            Assertions.assertInstanceOf(List.class, result);
-            for(Article article:result) {
-                System.out.println(article);
-            }
-
-            writer=new Writer(result.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-	   
-        System.exit(0);
-		//SpringApplication.run(K8SDemoApplication.class, args);
+		SpringApplication.run(K8SDemoApplication.class, args);
 	}
-
 }
